e82188bf50f2205ac530f6e4cac3a9f7f6804f23
Michael <116588587+mwcole1954@users.noreply.github.com>
2024-04-07 15:10:31 -0400

    -m "new changes to global_vars"

--- global_vars.py
+++ global_vars.py
@@ -5,17 +5,34 @@ import pathlib
 class GlobalVars:
     """Class of constants"""
 
-    # LABEL_FONT = ("Arial", 14, 'bold')
-    # FONT = ("Arial", 12, 'normal')
-    # EM_FONT = ("Arial", 12, 'bold')
+    def __init__(self):
+        # List of file names
+        self.txt_files_name = ['header', 'summary', 'ingredients', 'nutrients', 'instructions']
+        self.label_font = ("Arial", 14, 'bold')
+        self.font = ("Arial", 12, 'normal')
+        self.em_font = ("Arial", 12, 'bold')
+        # Current widget being affected
+        self.current_widget = ''
+        # Path to the Recipes folder
+        self.init_directory = pathlib.Path(os.getcwd(), "Recipes")
+        # The current recipe folder; if empty, then working on a new recipe and hasn't been saved
+        self.recipe_name = ''
+        # List of panel objects
+        self.panel_list = list()
 
-    current_widget = ''
+    def __repr__(self):
+        return GlobalVars.__dict__
 
-    # Path to the Recipes folder
-    init_directory = pathlib.Path(os.getcwd(), "Recipes")
-    # The current recipe folder; if empty, then working on a new recipe and hasn't been saved
-    recipe_name = ''
-    # List of panel objects
-    panel_list = []
-    # List of file names
-    txt_files_name = ['header', 'summary', 'ingredients', 'nutrients', 'instructions']
+    def update_panel_list(self, panel_name):
+        self.panel_list.append(panel_name)
+
+    def update_recipe_name(self, recipe_name):
+        self.recipe_name = recipe_name
+
+    def update_current_widget(self, widget):
+        self.current_widget = widget
+
+
+if __name__ == "__main__":
+    gv = GlobalVars()
+    print(gv.__repr__())
